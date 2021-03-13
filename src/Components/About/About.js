26a7fcfef7edd6bf2e018e2eef36bc97e639a78f
Patrick <zhen.patrick@gmail.com>
2021-03-12 20:16:31 -0500

    Changed about page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import "./About.css"
+import React from "react";
+import "./About.css";
+import logo from "./buffash.jpg";
+import holytrin from "./holytrinpoke.jpg";
 
 const About = () => {
-    return (
-        <div>
-            <h1>about</h1>
-        </div>
-    );
+  return (
+    <>
+      <main className="pokemon">
+        <h1>
+          Pokemon have protected the world from evil for time in immemorial. Do
+          you believe you have what it takes to be a pokemon Master?!
+        </h1>
+        <img src={logo} alt="buffash" />
+        <img src={holytrin} alt="holytrin" />
+      </main>
+    </>
+  );
 };
 
 export default About;
